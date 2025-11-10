9e6b0001ecbabab6d7b267bb12a64c86219a053c
ancuber1031 <ansona1131@gmail.com>
2025-11-10 11:56:51 +0800

    Add Mobius function

---
+++ codebook/6_Math/Mobius_function.cpp
@@ -0,0 +1,25 @@
+#include "include/common.h"
+vector<int> mobius_up_to(int N) {
+    vector<int> mu(N + 1, 1), prm;
+    vector<bool> is_prm(N + 1, 1);
+    mu[0] = 0;
+    for (int i = 2; i <= N; ++i) {
+        if (is_prm[i]) {
+            prm.push_back(i);
+            mu[i] = -1;
+        }
+        for (auto &p : prm) {
+            if (i * p > N) break;
+            is_prm[i * p] = 0;
+
+            if (i % p == 0) {
+                mu[i * p] = 0;
+                break;
+            } else {
+                mu[i * p] = -mu[i];
+            }
+        }
+    }
+    return mu;
+}
+
